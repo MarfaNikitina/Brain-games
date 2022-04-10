843638e97da8e0c0b04ebb23cf84fe056fa8ec87
Marfa Nikitina <marfik93@mail.ru>
2022-04-10 21:41:13 +0300

    10.04 v6

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,7 +4,7 @@ from brain_games.games.common import common_logic_of_games
 
 
 def main():
-    common_logic_of_games(calc)
+    common_logic_of_games(calc.expression, calc.true_answer, calc.RULE_OF_THE_GAME)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -2,9 +2,8 @@
 from brain_games.games import gcd
 from brain_games.games.common import common_logic_of_games
 
-
 def main():
-    common_logic_of_games(gcd)
+    common_logic_of_games(gcd.expression, gcd.true_answer, gcd.RULE_OF_THE_GAME)
 
 
 if __name__ == '__main__':
