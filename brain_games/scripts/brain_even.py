f504ef548e4c1ee76b8e2e40d9eb3ecd3b2419d2
Marfa Nikitina <marfik93@mail.ru>
2022-04-10 21:22:55 +0300

    10.04 v3

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,7 +4,7 @@ from brain_games.games.common import common_logic_of_games
 
 
 def main():
-    common_logic_of_games(even)
+    common_logic_of_games(expression, true_answer, RULE_OF_THE_GAME)
 
 
 if __name__ == '__main__':
