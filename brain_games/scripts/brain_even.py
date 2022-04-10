c405d5d3ce300544c302222219c5aad22a6a3db6
Marfa Nikitina <marfik93@mail.ru>
2022-04-10 21:27:10 +0300

    10.04 v4

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,6 +3,11 @@ from brain_games.games import even
 from brain_games.games.common import common_logic_of_games
 
 
+(expression, true_answer) = even()
+RULE_OF_THE_GAME = 'Answer "yes" if the number is even,' \
+                       ' otherwise answer "no".'
+
+
 def main():
     common_logic_of_games(expression, true_answer, RULE_OF_THE_GAME)
 
