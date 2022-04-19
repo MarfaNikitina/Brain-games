e8c97665b835f0e2608832dd763912e6860b8ab4
Marfa Nikitina <marfik93@mail.ru>
2022-04-19 17:56:38 +0300

    19/04 v1

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,10 +8,10 @@ def generate_question_and_answer():
     numb1 = randint(1, 100)
     numb2 = randint(1, 100)
     operators = {'+': operator.add,
-           '-': operator.sub,
-           '*': operator.mul}
+                 '-': operator.sub,
+                 '*': operator.mul}
     random_operator = choice(list(operators.keys()))
     correct_operator = operators[random_operator]
     correct_answer = str(correct_operator(numb1, numb2))
-    question = str(numb1) + ' ' + random_ops + ' ' + str(numb2)
+    question = str(numb1) + ' ' + random_operator + ' ' + str(numb2)
     return question, correct_answer
