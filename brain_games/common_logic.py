9a7e8950dd0aeb0c6e809c97028e7412ca131067
Marfa Nikitina <marfik93@mail.ru>
2022-04-19 17:54:33 +0300

    19/04

--- brain_games/common_logic.py
+++ brain_games/common_logic.py
@@ -4,12 +4,11 @@ MAX_COUNT_OF_TRY = 3
 
 
 def start(game):
-    rule_of_the_game = game.RULE
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     if name:
         print('Hello, {}!'.format(name))
-    print(rule_of_the_game)
+    print(game.RULE)
     for _ in range(MAX_COUNT_OF_TRY):
         (expression, true_answer) = (game.generate_question_and_answer())
         print(f'Question: {expression}')
--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,11 +7,11 @@ RULE = 'What is the result of the expression?'
 def generate_question_and_answer():
     numb1 = randint(1, 100)
     numb2 = randint(1, 100)
-    ops = {'+': operator.add,
+    operators = {'+': operator.add,
            '-': operator.sub,
            '*': operator.mul}
-    random_ops = choice(list(ops.keys()))
-    correct_operator = ops[random_ops]
+    random_operator = choice(list(operators.keys()))
+    correct_operator = operators[random_operator]
     correct_answer = str(correct_operator(numb1, numb2))
     question = str(numb1) + ' ' + random_ops + ' ' + str(numb2)
     return question, correct_answer
